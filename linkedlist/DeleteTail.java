569b3cccf6d2a6b7fba4e8473653fa47aefe78ed
ShiwaniCC <shiwani.soni@codeclouds.co.in>
2024-09-25 23:05:23 +0530

    Delete any node

--- linkedlist/DeleteTail.java
+++ linkedlist/DeleteTail.java
@@ -1,5 +1,6 @@
 public class DeleteTail {
       public static void main(String[] args) {
+            DeleteTail ob = new DeleteTail();
             int a[] = {1,2,3,4,5}, i;
             Node head = new Node(a[0]), temp;
             Node mover = head;
@@ -11,12 +12,39 @@ public class DeleteTail {
             }
             temp = head;
 
-            while (temp.next.next != null) {
+            head = ob.deleteNode(head, 3);
+            System.out.println("the list is:");
+            while (temp != null) {
+                  System.out.print(temp.data + " ");
                   temp = temp.next;
             }
-            temp.next = null;
+            System.out.println();
+            //deleting the tail
+            // while (temp.next.next != null) {
+            //       temp = temp.next;
+            // }
+            // temp.next = null;
 
-            System.out.println("now the tail element is: " + temp.data);
+            // System.out.println("now the tail element is: " + temp.data);
+      }
+
+      Node deleteNode(Node head, int k) {
+            Node temp = head, prev = temp;
+            int count = 0;
+            //we have to delete the head
+            if(k == 1) {
+                  head = head.next;
+            } 
+
+            while (temp != null) {
+                  count++;
+                  if(count == k) {
+                        prev.next = prev.next.next;
+                  }
+                  prev = temp;
+                  temp = temp.next;
+            }
+            return head;
       }
 }
 
