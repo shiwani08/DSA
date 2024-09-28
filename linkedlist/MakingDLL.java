3c1eb94e756f04b52dd62d97f67d1d6c17a74eeb
ShiwaniCC <shiwani.soni@codeclouds.co.in>
2024-09-29 00:24:02 +0530

    Node prev added

---
+++ linkedlist/MakingDLL.java
@@ -0,0 +1,49 @@
+public class MakingDLL {
+      public static void main(String[] args) {
+            MakingDLL ob = new MakingDLL();
+
+            int arr[] = {1, 2, 3, 4, 5};
+            Node head = new Node(arr[0]);
+            Node prev = new Node(arr[0]);
+            Node temp = head;
+            temp = ob.arrToDLL(head, arr);
+
+            ob.print(temp);
+      }
+
+      Node arrToDLL (Node head, int arr[]) {
+            Node temp = head, mover;
+            int i;
+            for (i = 1; i < arr.length; i++) {
+                  mover = new Node(arr[i]);
+                  temp.next = mover;
+                  temp = mover;
+            }
+
+            return head;
+      }
+      void print (Node head) {
+            Node temp = head;
+            while (temp != null) {
+                  System.out.print(temp.data + " ");
+                  temp = temp.next;
+            }
+            System.out.println();
+      }
+}
+
+class Node {
+      int data;
+      Node next, prev;
+
+      Node (int data1, Node next1, Node prev1) {
+            this.data = data1;
+            this.next = next1;
+            this.prev = prev1;
+      }
+      Node (int data1) {
+            this.data = data1;
+            this.next = null;
+            this.prev = null;
+      }
+}
