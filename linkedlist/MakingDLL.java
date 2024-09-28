0c84a3915e99596445e62fc30d6b6b2fd2f55d0f
ShiwaniCC <shiwanisoni29082002@gmail.com>
2024-09-29 00:29:57 +0530

    Node mover added

--- linkedlist/MakingDLL.java
+++ linkedlist/MakingDLL.java
@@ -5,6 +5,7 @@ public class MakingDLL {
             int arr[] = {1, 2, 3, 4, 5};
             Node head = new Node(arr[0]);
             Node prev = new Node(arr[0]);
+            Node mover = head;
             Node temp = head;
             temp = ob.arrToDLL(head, arr);
 
