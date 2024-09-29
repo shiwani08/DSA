dce8bde563b9d3954cd8e137393b6306361b9cfb
ShiwaniCC <shiwanisoni29082002@gmail.com>
2024-09-29 23:12:02 +0530

    deleting the head, tail in DLL but some compliation issues

--- src/linkedlist/DeleteInDLL.java
+++ src/linkedlist/DeleteInDLL.java
@@ -1,3 +1,79 @@
+package linkedlist;
+
 public class DeleteInDLL {
-      
+      public static void main(String[] args) {
+            DeleteInDLL ob = new DeleteInDLL();
+            MakingDLL ob2 = new MakingDLL();
+
+            int a[] = { 1, 2, 3, 4, 5 };
+            Node head = new Node(a[0]);
+            Node temp = head;
+            ob2.arrToDLL(head, a);
+
+            ob2.print(head);
+
+            ob.DeleteHead(head);
+            ob2.print(head);
+      }
+
+      Node DeleteHead(Node head) {
+            Node prev = head;
+
+            if (head == null || head.next == null)
+                  return null;
+
+            head = head.next;
+            head.prev = null;
+            prev.next = null;
+
+            return head;
+      }
+
+      Node DeleteTail(Node head) {
+            Node tail = head, prev = head;
+
+            if(head == null) 
+            return null;
+
+            while (tail != null) {
+                  tail = tail.next;
+            }
+            prev = tail.prev;
+            prev.next = null;
+            tail.prev = null;
+            return head;
+      }
+
+      Node DeleteAnyElement(Node head) {
+            Node temp = head;
+            return temp;
+      }
+
+      Node DeleteAtAnyNode(Node head, int pos) {
+            Node temp = head;
+            int count = 0;
+            if (head == null) 
+            return null;
+
+            // while 
+            return temp;
+      }
+
+}
+
+class Node {
+      int data;
+      Node next, prev;
+
+      Node(int data1, Node next1, Node prev1) {
+            this.data = data1;
+            this.next = next1;
+            this.prev = prev1;
+      }
+
+      Node(int data1) {
+            this.data = data1;
+            this.next = null;
+            this.prev = null;
+      }
 }
--- src/linkedlist/MakingDLL.java
+++ src/linkedlist/MakingDLL.java
@@ -1,13 +1,16 @@
 package linkedlist;
-import linkedlist.ArrayToLL;
 
 public class MakingDLL {
       public static void main(String[] args) {
             MakingDLL ob = new MakingDLL();
-            ArrayToLL ob2 = new ArrayToLL();
 
             int arr[] = {1, 2, 3, 4, 5};
-            Node head = new Node(arr[0]);
+            if (arr.length == 0) {
+                  System.out.println("Array is empty.");
+                  return;
+            }
+
+            Node head = new Node(arr[0]);  // Initialize the head of the list
             Node temp = head;
             temp = ob.arrToDLL(head, arr);
 
@@ -15,19 +18,19 @@ public class MakingDLL {
       }
 
       Node arrToDLL (Node head, int arr[]) {
-            Node temp = head, mover;
-            int i;
-            for (i = 1; i < arr.length; i++) {
-                  mover = new Node(arr[i]);
-                  temp.next = mover;
-                  temp = mover;
+            Node temp = head;
+            for (int i = 1; i < arr.length; i++) {  // Start from second element
+                  Node mover = new Node(arr[i]);  // Create a new node
+                  temp.next = mover;  // Link the current node to the new node
+                  mover.prev = temp;  // Link back to the previous node
+                  temp = mover;  // Move the temp pointer forward
             }
-
             return head;
       }
+
       void print (Node head) {
             Node temp = head;
-            while (temp != null) {
+            while (temp != null) {  // Traverse and print the list
                   System.out.print(temp.data + " ");
                   temp = temp.next;
             }
@@ -39,12 +42,13 @@ class Node {
       int data;
       Node next, prev;
 
-      Node (int data1, Node next1, Node prev1) {
+      Node(int data1, Node next1, Node prev1) {
             this.data = data1;
             this.next = next1;
             this.prev = prev1;
       }
-      Node (int data1) {
+
+      Node(int data1) {
             this.data = data1;
             this.next = null;
             this.prev = null;
